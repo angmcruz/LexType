498cfc98db46f1c7fc2e418c560d514737d3d548
angmcruz <108002776+angmcruz@users.noreply.github.com>
2025-06-13 19:31:29 -0500

    Subiendo algoritmo para prueba

---
+++ algoritmo2.ts
@@ -0,0 +1,21 @@
+
+/* Melissa Cruz */ 
+//Algoritmo para verificar si un map y un set tienen los mismos elementos
+let frutasSet: Set<string> = new Set(["manzana", "pera", "uva"]);
+let frutasMap: Map<string, number> = new Map([
+ ["manzana", 1],
+ ["pera", 2],
+ ["uva", 3],
+]);
+function tienenMismosElementos(set: Set<string>, map: Map<string, number>): boolean {
+ if (set.size !== map.size) return false;
+ for (let item of set) {
+ if (!map.has(item)) return false;
+ }
+ return true;
+}
+if (tienenMismosElementos(frutasSet, frutasMap)) {
+ console.log("El Set y el Map tienen los mismos elementos.");
+} else {
+ console.log("El Set y el Map son diferentes.");
+}
